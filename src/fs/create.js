257ac6896561411ca1e4de395a61cb19ca4f1ac5
Alexey Tarasov <alexey@avsoft.ru>
2022-11-22 23:11:54 +0300

    feat: creat function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import { writeFile } from "node:fs/promises";
+import path from "path";
+
 const create = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    await writeFile(
+      path.join("src", "fs", "files", "fresh.txt"),
+      "I am fresh and young",
+      { flag: "wx" }
+    );
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await create();
--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,16 +1,12 @@
 import { unlink } from "node:fs/promises";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
+import path from "path";
 
 const remove = async () => {
   // Write your code here
-  const __dirname = dirname(fileURLToPath(import.meta.url));
   try {
-    let link = path.join(__dirname, "files", "fileToRemove.txt");
-    await unlink(link);
-    console.log(`successfully deleted ${link}`);
+    await unlink(path.join("src", "fs", "files", "fileToRemove.txt"));
   } catch (error) {
-    console.error("Error deleting file happened:", error.message);
+    throw new Error("FS operation failed");
   }
 };
 
