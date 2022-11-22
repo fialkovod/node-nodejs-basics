5b40e619a865fa965860919ecd5e28dccd770db6
Alexey Tarasov <alexey@avsoft.ru>
2022-11-22 20:12:56 +0300

    feat: delete file function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import { unlink } from "node:fs/promises";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
 const remove = async () => {
-    // Write your code here 
+  // Write your code here
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  try {
+    let link = path.join(__dirname, "files", "fileToRemove.txt");
+    await unlink(link);
+    console.log(`successfully deleted ${link}`);
+  } catch (error) {
+    console.error("Error deleting file happened:", error.message);
+  }
 };
 
 await remove();
