e6a2ee5e8dd9f165b33cc961f85bd1c94d6777d0
Alexey Tarasov <alexey@avsoft.ru>
2022-11-23 19:30:15 +0300

    feat: add copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,17 @@
+import { cp } from "node:fs/promises";
+import { fileURLToPath } from "url";
+import path from "path";
+
 const copy = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    let mod_dir = path.dirname(fileURLToPath(import.meta.url));
+    let from = path.join(mod_dir, "files");
+    let to = path.join(mod_dir, "files_copy");
+    await cp(from, to, { force: false, errorOnExist: true, recursive: true });
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 copy();
