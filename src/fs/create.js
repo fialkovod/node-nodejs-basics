10e8577d7fb074e3b8890ae74473eaaf2b2394bd
Alexey Tarasov <alexey@avsoft.ru>
2022-11-23 19:32:35 +0300

    fix: create function use dirname

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,11 +1,13 @@
 import { writeFile } from "node:fs/promises";
+import { fileURLToPath } from "url";
 import path from "path";
 
 const create = async () => {
   // Write your code here
   try {
+    let mod_dir = path.dirname(fileURLToPath(import.meta.url));
     await writeFile(
-      path.join("src", "fs", "files", "fresh.txt"),
+      path.join(mod_dir, "files", "fresh.txt"),
       "I am fresh and young",
       { flag: "wx" }
     );
