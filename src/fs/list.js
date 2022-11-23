b16710670f87cf061d686e3f9aeac02c5d0b9314
Alexey Tarasov <alexey@avsoft.ru>
2022-11-23 19:51:05 +0300

    feat: add list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+import { readdir } from "node:fs/promises";
+import { fileURLToPath } from "url";
+import path from "path";
+
 const list = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    const mod_dir = path.dirname(fileURLToPath(import.meta.url));
+    const files = await readdir(path.join(mod_dir, "files"));
+    console.log(files);
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await list();
