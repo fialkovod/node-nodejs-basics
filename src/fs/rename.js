9b569617223aaa65704f5496785bbc864dfc9a53
Alexey Tarasov <alexey@avsoft.ru>
2022-11-23 19:43:43 +0300

    feat: add rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,17 @@
+import { rename as frename } from "node:fs/promises";
+import { fileURLToPath } from "url";
+import path from "path";
+
 const rename = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    let mod_dir = path.dirname(fileURLToPath(import.meta.url));
+    let oldName = path.join(mod_dir, "files", "wrongFilename.txt");
+    let newName = path.join(mod_dir, "files", "properFilename.md");
+    await frename(oldName, newName);
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await rename();
