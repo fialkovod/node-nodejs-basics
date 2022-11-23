d0939ef02e763a4570b171d30a2a6ddd4d88326a
Alexey Tarasov <alexey@avsoft.ru>
2022-11-23 20:28:06 +0300

    feat: add args parse function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+  // Write your code here
+  const ar = process.argv;
+  let res = [];
+  for (let i = 2; i < ar.length; i = i + 2) {
+    if (ar[i].startsWith("--")) {
+      res.push(`${ar[i].slice(2)} is ${ar[i + 1]}`);
+    }
+  }
+  console.log(res.join(", "));
 };
 
 parseArgs();
