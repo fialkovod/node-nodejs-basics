31908bc8d67fcfca4b48e0c39a21b66d7ce864b2
Alexey Tarasov <alexey@avsoft.ru>
2022-11-23 20:12:49 +0300

    feat: add env variables parse function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+  // Write your code here
+  const envs = process.env;
+  let res = [];
+  for (let en in envs) {
+    if (en.indexOf("RSS_") === 0) res.push(`${en}=${envs[en]}`);
+  }
+  console.log(res.join("; "));
 };
 
 parseEnv();
