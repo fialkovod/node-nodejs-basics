e1bbf45596422f910322b0c6e17fd612ceb00116
Alexey Tarasov <alexey@avsoft.ru>
2022-11-23 19:55:03 +0300

    feat: add read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import { readFile } from "node:fs/promises";
+import { fileURLToPath } from "url";
+import path from "path";
+
 const read = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    const mod_dir = path.dirname(fileURLToPath(import.meta.url));
+    const fileContent = await readFile(
+      path.join(mod_dir, "files", "fileToRead.txt"),
+      { encoding: "utf8" }
+    );
+    console.log(fileContent);
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await read();
