60e6a17023555a39d18803a0e510578e023c2426
Alexey Tarasov <alexey@avsoft.ru>
2022-11-24 15:21:16 +0300

    feat: module refactor

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,6 +1,6 @@
 import path from "path";
 import { release, version } from "os";
-import { createServer } from "http";
+import { createServer as createServerHttp } from "http";
 import { fileURLToPath } from "url";
 
 import "./files/c.js";
@@ -10,12 +10,16 @@ const __dirname = path.dirname(__filename);
 
 const random = Math.random();
 
-export let unknownObject;
+let unknownObject;
 
 if (random > 0.5) {
-  unknownObject = await import("./files/a.json", { assert: { type: "json" } });
+  unknownObject = (await import("./files/a.json")).default;
 } else {
-  unknownObject = await import("./files/b.json", { assert: { type: "json" } });
+  unknownObject = (
+    await import("./files/b.json", {
+      assert: { type: "json" },
+    })
+  ).default;
 }
 
 console.log(`Release ${release()}`);
@@ -25,7 +29,7 @@ console.log(`Path segment separator is "${path.sep}"`);
 console.log(`Path to current file is ${__filename}`);
 console.log(`Path to current directory is ${__dirname}`);
 
-export const myServer = createServer((_, res) => {
+const myServer = createServerHttp((_, res) => {
   res.end("Request accepted");
 });
 
@@ -37,3 +41,5 @@ myServer.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
   console.log("To terminate it, use Ctrl+C combination");
 });
+
+export { myServer, unknownObject };
