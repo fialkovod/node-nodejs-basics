4232a4322233f2222a006c235a38d31f080e8d1b
Alexey Tarasov <alexey@avsoft.ru>
2022-11-24 23:18:17 +0300

    feat: add read stream function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,10 @@
+import { createReadStream } from "fs";
+import { createHash } from "crypto";
 const calculateHash = async () => {
-    // Write your code here 
+  // Write your code here
+  const hash = createHash("sha256");
+  const fromfile = createReadStream("./files/fileToCalculateHashFor.js");
+  fromfile.pipe(hash).setEncoding("hex").pipe(console.log);
 };
 
 await calculateHash();
--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,10 @@
+import { open } from "fs/promises";
+
 const read = async () => {
-    // Write your code here 
+  // Write your code here
+  const fd = await open("./src/streams/files/fileToRead.txt");
+  const readable = fd.createReadStream();
+  readable.pipe(process.stdout);
 };
 
 await read();
