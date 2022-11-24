eb159040d16a6ccf021ad9674bd60b35fca97a58
Alexey Tarasov <alexey@avsoft.ru>
2022-11-25 01:29:33 +0300

    add calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,10 +1,16 @@
-import { createReadStream } from "fs";
+import { readFile } from "node:fs/promises";
 import { createHash } from "crypto";
 const calculateHash = async () => {
   // Write your code here
   const hash = createHash("sha256");
-  const fromfile = createReadStream("./files/fileToCalculateHashFor.js");
-  fromfile.pipe(hash).setEncoding("hex").pipe(console.log);
+  const fileContent = await readFile(
+    "./src/hash/files/fileToCalculateHashFor.txt",
+    {
+      encoding: "utf8",
+    }
+  );
+  hash.update(fileContent);
+  console.log(hash.digest("hex"));
 };
 
 await calculateHash();
