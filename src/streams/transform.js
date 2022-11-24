5af4276dfe465586548dbfcf2b9d33a7d521a9a4
Alexey Tarasov <alexey@avsoft.ru>
2022-11-25 01:21:44 +0300

    feat: add transform stream function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+import { Transform } from "stream";
+
 const transform = async () => {
-    // Write your code here 
+  // Write your code here
+
+  const reverseStr = new Transform({
+    transform(chunk, encoding, callback) {
+      this.push(String(chunk).split("").reverse().join("") + "\n");
+      callback();
+    },
+  });
+
+  process.stdin.pipe(reverseStr).pipe(process.stdout);
 };
 
 await transform();
